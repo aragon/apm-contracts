74150d6d6f708665751ebcc3591a5b8aebcdfe93
Jorge Izquierdo <jorge@izqui.me>
2017-11-09 14:06:51 +0100

    Use DelegateProxy as dependency from aragon-core

--- contracts/ForwarderFactory.sol
+++ contracts/ForwarderFactory.sol
@@ -1,28 +1,6 @@
 pragma solidity ^0.4.15;
 
-contract DelegateProxy {
-    /**
-    * @dev Performs a delegatecall and returns whatever the delegatecall returned (entire context execution will return!)
-    * @param _dst Destination address to perform the delegatecall
-    * @param _calldata Calldata for the delegatecall
-    */
-    function delegatedFwd(address _dst, bytes _calldata) internal {
-        assembly {
-            switch extcodesize(_dst) case 0 { revert(0, 0) }
-
-            let result := delegatecall(sub(gas, 10000), _dst, add(_calldata, 0x20), mload(_calldata), 0, 0)
-            let size := returndatasize
-
-            let ptr := mload(0x40)
-            returndatacopy(ptr, 0, size)
-
-            // revert instead of invalid() bc if the underlying call failed with invalid() it already wasted gas.
-            // if the call returned error data, forward it
-            switch result case 0 { revert(ptr, size) }
-            default { return(ptr, size) }
-        }
-    }
-}
+import "@aragon/core/contracts/common/DelegateProxy.sol";
 
 contract ForwarderFactory {
     event Deployed(address forwarder);
