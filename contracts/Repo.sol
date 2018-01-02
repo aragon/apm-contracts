db32af0b4cb6cc8f7eb5814daa152d4d04dce2b1
Oliver Nordbjerg <hi@notbjerg.me>
2018-01-02 01:41:41 +0100

    Turn Repo into an app

--- contracts/Repo.sol
+++ contracts/Repo.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.4.15;
 
-import "./Ownable.sol";
+import "@aragon/core/contracts/apps/App.sol";
 
-contract Repo is Ownable {
+contract Repo is App {
     struct Version {
         uint16[3] semanticVersion;
         address contractAddress;
@@ -13,6 +13,8 @@ contract Repo is Ownable {
     mapping (bytes32 => uint256) versionIdForSemantic;
     mapping (address => uint256) latestVersionIdForContract;
 
+    bytes32 public constant NEW_VERSION_ROLE = bytes32(1);
+
     event NewVersion(uint256 versionId, uint16[3] semanticVersion);
 
     /**
@@ -21,7 +23,11 @@ contract Repo is Ownable {
     * @param _contractAddress address for smart contract logic for version (if set to 0, it uses last versions' contractAddress)
     * @param _contentURI External URI for fetching new version's content
     */
-    function newVersion(uint16[3] _newSemanticVersion, address _contractAddress, bytes _contentURI) onlyOwner {
+    function newVersion(
+        uint16[3] _newSemanticVersion,
+        address _contractAddress,
+        bytes _contentURI
+    ) auth(NEW_VERSION_ROLE) {
         if (versions.length > 0) {
             Version storage lastVersion = versions[versions.length - 1];
             require(isValidBump(lastVersion.semanticVersion, _newSemanticVersion));
